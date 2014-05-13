79ca4caf75004a1127a2c44a33b9e3f72d148c30
Paul Hoffman <paul@flo.org>
2014-05-12 23:58:41 -0400

    try to do without custom includes

--- lib/Biblio/NCIP1/Common.pm
+++ lib/Biblio/NCIP1/Common.pm
@@ -1,5 +1,5 @@
 # Set up the right environment
 
-use lib qw(lib /usr/lib/perl5/vendor_perl/5.8.8 /home/voyager/lib/perl5lib /home/voyager/proj/vsip/lib);
+#use lib qw(lib /usr/lib/perl5/vendor_perl/5.8.8 /home/voyager/lib/perl5lib /home/voyager/proj/vsip/lib);
 
 1;
